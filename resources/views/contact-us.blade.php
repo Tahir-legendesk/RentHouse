5113232b74029d404b2c500484c1b0feaa8bd4f2
imzainmalik <zain.malik@legendesk.com>
2023-02-22 23:53:40 +0500

    new changes

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -23,11 +23,6 @@
                         </div>
                     </div>
                     
-                    <div class="alert alert-success"></div>
-                    <div class="alert alert-danger"></div>
-                    <div class="alert alert-warning"></div>
-                    <div class="alert alert-info"></div>
-                    <div class="alert alert-primary"></div>
 
                     <div class="col-md-6 wow fadeInUp" data-wow-delay="0.25s">
                         <div class="cont_2">
